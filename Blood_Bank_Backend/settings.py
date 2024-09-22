50dc5e5e74acb4c9c65abcf4740b27af21934410
salauddin85 <ahmedsalauddin677785@gmail.com>
2024-09-22 19:44:42 +0600

    done

--- Blood_Bank_Backend/settings.py
+++ Blood_Bank_Backend/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0$ij#(&4cd_&1ojqsrpq(+v2fj^m@9+ed-at%w&u&e_g!t@^87'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 CORS_ALLOW_ALL_ORIGINS = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['blood-bank-backend-c7w8.onrender.com', '127.0.0.1']
+
 CSRF_TRUSTED_ORIGINS = [
     'https://blood-bank-backend-c7w8.onrender.com',
     'http://blood-bank-backend-c7w8.onrender.com',
